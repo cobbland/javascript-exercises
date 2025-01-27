98dafc87c73de9b7f4ead5cd3dbb81de60790b45
cobbland <jacobdensford@proton.me>
2025-01-27 18:06:30 -0500

    Solve reverseString
    Technically. My solution didn't match the one given.
    I wrote a loop to do the work. The given solution split
    the string into an array of letters, reverse the array,
    and then joined them back into a string. Much more elegant,
    but mine still got the job done. (Apparently I need to
    start remembering all these random methods though.)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(oldString) {
+    let newString = "";
+    let stringLength = oldString.length - 1;
+    while (stringLength >= 0) {
+        newString += oldString[stringLength];
+        stringLength--;
+    }
+    return newString;
 };
 
 // Do not edit below this line
