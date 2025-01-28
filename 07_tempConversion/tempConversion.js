c888aaff6f229be9dd31a63ab2cbaedc2b764c9f
cobbland <jacobdensford@proton.me>
2025-01-27 21:46:53 -0500

    Solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+// F -> C: (32°F − 32) × 5/9 = 0°C
+
+const convertToCelsius = function(temp) {
+  return Math.round(((temp - 32) * (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+// C -> F: (0°C × 9/5) + 32 = 32°F
+
+const convertToFahrenheit = function(temp) {
+  return Math.round(((temp * (9/5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
