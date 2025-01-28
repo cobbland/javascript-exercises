f2b902a72be1c3c2db8ca3277593291805dba304
cobbland <jacobdensford@proton.me>
2025-01-27 21:01:50 -0500

    Solve removeFromArray
    Again, didn't do it exactly how the presented solution did.
    I went a more roundabout way, because I wasn't familiar with
    rest parameters or the forEach method. But I still got
    the job done.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, toBeRemoved) {
+    for (arg in arguments) {
+        if (arg === 0 || myArray.indexOf(arguments[arg]) === -1){
+            continue;
+        } while (myArray.indexOf(arguments[arg]) !== -1) {
+            myArray.splice(myArray.indexOf(arguments[arg]), 1);
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
