285c962a70d6ae82113d78b1645e99da60a1add2
cobbland <jacobdensford@proton.me>
2025-01-27 21:25:10 -0500

    Solve sumAll
    Mine was almost the same as the given solution this time, but
    perhaps I still could have done things in a few few lines of code.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if ((numOne < 0 || !Number.isInteger(numOne)) ||
+        (numTwo < 0 || !Number.isInteger(numTwo))) {
+            return "ERROR";
+        }
+    let highNum;
+    let lowNum;
+    if (numOne >= numTwo) {
+        highNum = numOne;
+        lowNum = numTwo;
+    } else {
+        highNum = numTwo;
+        lowNum = numOne;
+    }
+    let finalNum = 0;
+    let numCount = lowNum;
+    while (numCount <= highNum) {
+        finalNum += numCount;
+        numCount++;
+    }
+    return finalNum;
 };
 
 // Do not edit below this line
