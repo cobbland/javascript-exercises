258a81f990f6ada02db14093c3bdf2a65e3e3488
cobbland <jacobdensford@proton.me>
2025-02-01 14:05:29 -0500

    Solve rest of problems
    Better today than yesterday, but still not perfect.
    I need to get better at all these array methods.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (person in people) {
+        if (!Object.hasOwn(people[person], "yearOfDeath")) {
+            people[person].yearOfDeath = new Date().getFullYear();
+        }
+    }
+    people.sort((personA, personB) => {
+        return (personA.yearOfDeath - personA.yearOfBirth)
+            - (personB.yearOfDeath - personB.yearOfBirth);
+    });
+    return people[people.length - 1];
 };
 
 // Do not edit below this line
